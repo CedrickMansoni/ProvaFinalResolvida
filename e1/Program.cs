f4a3e8d3989f9437d3fe80a9a6318117a1fb3f8f
Cedrick Mansoni <cedrick.k.mansoni@gmail.com>
2024-09-07 22:14:09 +0100

    Modificação do exercício E1, add comentários e troca da estrutura for pela foreach

--- e1/Program.cs
+++ e1/Program.cs
@@ -1,18 +1,22 @@
 ﻿/*
-    Crie um programa que leia 8 numeros
+    Crie um programa que leia 8 números
     inteiros de um vector e exiba o maior 
     e o menor numero.
 */
-int[] num = new int[8];
-for (int i = 0; i < num.Length; i++) 
+int[] num = new int[8]; // Declaração do vector de 8 posições
+
+int contador = 0; // Declaração de um contador para manipulação dos índices do vector
+
+foreach (int item in num) // Estrutura de repetição para podermos inserir valores ao vector  
 {
-    Console.WriteLine("Digite um numero inteiro");
-    num[i] = Convert.ToInt32(Console.ReadLine());
-    Console.Clear();
+    Console.WriteLine("Digite um numero inteiro");  // Solicitar ao usuário que informe os valores inteiros que serão armazenados no vector
+    num[contador] = Convert.ToInt32(Console.ReadLine()); // Atribuir esses valores nas posições do vector em função do contador
+    Console.Clear(); // Limpar a tela do console
+    contador++;   // Incrementar o valor do contador
 }
 
-int maior = num.Max();
-int menor = num.Min();
+int maior = num.Max(); // Encontrar o maior valor numerico inserido no vector 
+int menor = num.Min(); // Encontrar o menor valor numerico inserido no vector 
 
-Console.WriteLine($"O maior numero do vector e: {maior}");
-Console.WriteLine($"O menor numero do vector e: {menor}");
+Console.WriteLine($"O maior numero do vector é: {maior}\n");  // Imprimir na tela o maior valor
+Console.WriteLine($"O menor numero do vector é: {menor}");  // Imprimir na tela o menor valor
