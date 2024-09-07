eca0b9e6f1b7e8bfe81aaaa69f533be21be73902
Cedrick Mansoni <cedrick.k.mansoni@gmail.com>
2024-09-08 00:50:24 +0100

    Modificação do E4, add comentários

--- e4/Program.cs
+++ e4/Program.cs
@@ -2,11 +2,18 @@
 Crie um programa que leia 10 numeros inteiros 
 de um vector e calcule a soma dos numeros pares
 */
-int[] num = new int[10];
-int soma = 0;
-for(int i = 0; i < num.Length; i++)
+int[] num = new int[10]; // Declaração do vector de 10 posições
+
+int soma = 0; // Declaração da variável soma
+
+for(int i = 0; i < num.Length; i++) // Estrutura de repetição para podermos inserir valores aleatórios ao vector  
 {
-    num[i] = new Random().Next(0, 100);
-    if(num[i] % 2 is 0){soma += num[i]; Console.WriteLine($"Valor par sorteado {num[i]}");}
+    num[i] = new Random().Next(0, 100); // Atribuição de valores aleatórios gerados usando novas instâncias da class Random 
+
+    if(num[i] % 2 is 0) // Estrutura de decisão, caso o valor seja par, então incremente ele a variável soma
+    {
+        soma += num[i]; // Incrementar o valor par a varável soma
+        Console.WriteLine($"Valor par sorteado {num[i]}"); // Imprimir na tela o valor par sorteado
+    } 
 }
-Console.WriteLine($"\n\nA soma dos numeros pares sorteados e: {soma}");
+Console.WriteLine($"\n\nA soma dos números pares sorteados é: {soma}"); // Imprimir na tela a soma dos valores pares sorteados
